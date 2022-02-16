16120a5e771802dfc2921b72c7f97f9a24dd9cfe
injoon <injoon5@naver.com>
2022-02-16 17:27:20 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -5,8 +5,8 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 // You might need to insert additional domains in script-src if you are using external services
 const ContentSecurityPolicy = `
   default-src 'self';
-  script-src 'self' 'unsafe-eval' 'unsafe-inline' giscus.app;
-  style-src 'self' 'unsafe-inline' cdn.jsdelivr.net;
+  script-src 'self' 'unsafe-eval' 'unsafe-inline' app.forestry.io giscus.app use.fortawesome.com;
+  style-src 'self' 'unsafe-inline' app.forestry.io cdn.jsdelivr.net fonts.googleapis.com;
   img-src * blob: data:;
   media-src 'none';
   connect-src *;
